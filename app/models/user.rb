4f5bf11d6dcd14b32cc39c9caffecee94cc3aa51
MatthewR <matthewrice13@gmail.com>
2017-04-20 17:31:48 +0000

    Add password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,94 +1,83 @@
-class User < ApplicationRecord
+class User < ActiveRecord::Base
   attr_accessor :remember_token, :activation_token, :reset_token
-
-  before_validation :ensure_token
-  before_save   :downcase_email
-  before_create :create_activation_digest
   
+  before_save :downcase_email
+  before_create :create_activation_digest
+
   validates :name, presence: true, length: { maximum: 50 }
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 255 },format: { with: VALID_EMAIL_REGEX },uniqueness: { case_sensitive: false }
- 
-  has_secure_password
+  validates :email,
+            presence: true, length: { maximum: 255 },
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: { case_sensitive: false }
+
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
-  validates :token, uniqueness: true
+  has_secure_password
+
+  has_many :microposts, dependent: :destroy
   
 
   def self.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
-  
+
   def self.new_token
     SecureRandom.urlsafe_base64
   end
-  
+
   def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
   end
-  
+
   def authenticated?(attribute, token)
     digest = send("#{attribute}_digest")
     return false if digest.nil?
     BCrypt::Password.new(digest).is_password?(token)
   end
-  
+
   def forget
     update_attribute(:remember_digest, nil)
-  end  
+  end
 
-  
   def activate
-    update_attribute(:activated,    true)
+    update_attribute(:activated, true)
     update_attribute(:activated_at, Time.zone.now)
   end
 
-
-  def send_activation_email(ember_url: false)
-    if ember_url
-      activate
-    else
-      UserMailer.account_activation(self).deliver_now
-    end
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
   end
-  
-  
+
   def create_reset_digest
     self.reset_token = User.new_token
-    update_columns(reset_digest: true, reset_sent_at: Time.zone.now)
+    update_attribute(:reset_digest, User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
   end
 
   def send_password_reset_email
     UserMailer.password_reset(self).deliver_now
   end
-  
+
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
   end
-  
+
+  def feed
+    Micropost.where('user_id = ?', id)
+  end
 
   private
+
+  def downcase_email
+    self.email = email.downcase
+  end
+
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
+  end
   
-    def downcase_email
-      self.email = email.downcase
-    end
-
-    def create_activation_digest
-      self.activation_token  = User.new_token
-      self.activation_digest = User.digest(activation_token)
-    end
-
-    def ensure_token
-      self.token = generate_hex(:token) unless token.present?
-    end
-
-    def generate_hex(column)
-      loop do
-        hex = SecureRandom.hex
-        break hex unless self.class.where(column => hex).any?
-      end
-    end
-
 end
