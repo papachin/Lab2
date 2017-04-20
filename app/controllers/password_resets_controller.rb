7f3398d066e2bd96c685a1c8b32f157a3de21ba8
MatthewR <matthewrice13@gmail.com>
2017-04-20 16:46:44 +0000

    Add password

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -48,8 +48,8 @@ class PasswordResetsController < ApplicationController
     end
 
     def valid_user
-      unless (@user && @user.activated? &&
-              @user.authenticated?(:reset, params[:id]))
+      user = User.find_by(email: params[:email])
+      unless (user && user.activated? && user.authenticated?(:reset, params[:id]))
         redirect_to root_url
       end
     end
