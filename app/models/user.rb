3567f486f6f6ccf113481ed6c3497062eaa6529b
MatthewR <matthewrice13@gmail.com>
2017-04-20 15:59:44 +0000

    Add password

--- app/models/user.rb
+++ app/models/user.rb
@@ -37,7 +37,8 @@ class User < ApplicationRecord
 
   
   def activate
-    update_columns(activated: true, activated_at: Time.zone.now)
+    update_attribute(:activated,    true)
+    update_attribute(:activated_at, Time.zone.now)
   end
 
   def send_activation_email
